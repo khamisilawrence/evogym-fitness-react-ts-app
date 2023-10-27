200d9ee3e0cec293553e0508f292876e6bd4d4f8
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-27 10:13:20 -0500

    added logic

--- gym-typescript-app/src/scenes/navbar/index.tsx
+++ gym-typescript-app/src/scenes/navbar/index.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react"
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
+import Link from "./Link.js"
 
-type Props = {}
+type NavbarProps = {
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage, setSelectedPage}: NavbarProps) => {
   const flexBetween = "flex items-center justify-between"
   return (
     <nav>
@@ -16,10 +20,26 @@ const Navbar = (props: Props) => {
             {/* RIGHT SIDE OF NAVBAR */}
             <div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} gap-8 text-sm`}>
-                <p>Home</p>
-                <p>Benefits</p>
-                <p>Our Classes</p>
-                <p>Contact Us</p>
+                <Link 
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage} 
+                />
+                <Link 
+                page="Benefits"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage} 
+                />
+                <Link 
+                page="Our Classes"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage} 
+                />
+                <Link 
+                page="Contact Us"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage} 
+                />
               </div>
               <div className={`${flexBetween} gap-8`}>
                 <p>Sign In</p>
