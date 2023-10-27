f49e3a914a583cdb503656bd08897625de8dd430
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-27 10:16:50 -0500

    imported SelectedPage enum

--- gym-typescript-app/src/App.tsx
+++ gym-typescript-app/src/App.tsx
@@ -1,12 +1,6 @@
 import Navbar from "./scenes/navbar/index.js";
 import { useState } from "react";
-
-enum SelectedPage {
-  Home = "home",
-  Benefits = "benefits",
-  OurClasses = "ourclasses",
-  ContactUs = "contactus"
-}
+import { SelectedPage } from "@/shared/types.js";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
