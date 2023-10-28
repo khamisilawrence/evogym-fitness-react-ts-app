ad3590da6ed7e8764ef18f2eb2404c2c6ff1d180
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-27 19:28:01 -0500

    added logic

--- gym-typescript-app/src/App.tsx
+++ gym-typescript-app/src/App.tsx
@@ -1,14 +1,29 @@
 import Navbar from "./scenes/navbar/index.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types.js";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      else if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="app bg-gray-20">
       <Navbar 
-      selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
     </div>
   );
