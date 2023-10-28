3408a46f1e8cf219aee6035537ae9f3678a491ba
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-28 13:20:37 -0500

    added logic

--- gym-typescript-app/src/scenes/benefits/index.tsx
+++ gym-typescript-app/src/scenes/benefits/index.tsx
@@ -1,10 +1,29 @@
 import { SelectedPage } from "@/shared/types.js";
+import HText from "@/shared/HText.js";
 import { 
   HomeModernIcon, 
   UserGroupIcon, 
   AcademicCapIcon 
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description: "Neque adipiscing amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description: "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description: "Fusce vestibulum aliquam ut cras. Nisl lectus egestas nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+]
 
 type BenefitsProps = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -19,8 +38,19 @@ const Benefits = ({setSelectedPage}: BenefitsProps) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div>
-          .
+        {/* HEADER */}
+        <div className="md:my-5 md:w-3/5">
+          <HText>MORE THAN JUST A GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care for each and every member.
+          </p>
+        </div>
+
+        {/* BENEFITS */}
+        <div className="mt-5 items-center justify-between gap-8 md:flex">
+          {benefits.map((benefit) => (
+            <Benefit />
+          ))}
         </div>
       </motion.div>
     </section>
