7a60170ce04fcf79dba468d0b187ba741e83cb87
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-30 02:46:06 -0500

    completed content

--- gym-typescript-app/src/scenes/footer/index.tsx
+++ gym-typescript-app/src/scenes/footer/index.tsx
@@ -4,15 +4,28 @@ const Footer = () => {
   return (
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+
         <div className="mt-16 basis-1/2 md:mt-0">
           <img alt="Logo" src={Logo} />
           <p className="my-5">
-            Lorem ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque.
+          Evolutionary Fitness. Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studios to get the Body Shapes that you Dream of. Get Your Dream Body Now.
           </p>
           <p>© Evogym All Rights Reserved.</p>
         </div>
-        <div></div>
-        <div></div>
+
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Et gravida id et etiam</p>
+          <p className="my-5">Ullamcorper vivamus</p>
+        </div>
+
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+          <p>(333)425-6825</p>
+        </div>
+
       </div>
     </footer>
   )
