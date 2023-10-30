006754bf55938d989808b6c2c5ca551288886013
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-30 00:18:05 -0500

    added logic

--- gym-typescript-app/src/scenes/ourClasses/index.tsx
+++ gym-typescript-app/src/scenes/ourClasses/index.tsx
@@ -70,7 +70,7 @@ const OurClasses = ({setSelectedPage}: Props) => {
         </motion.div>
         <div className='mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden'>
           <ul className='w-[2800px] whitespace-nowrap'>
-            {OurClasses.map((item: ClassType, index) => (
+            {classes.map((item: ClassType, index) => (
               <Class
                 key={`${item.name}-${index}`}
                 name={item.name}
