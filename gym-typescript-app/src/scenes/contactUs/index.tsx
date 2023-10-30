fe6e31f49424525d68e1cffa9b9dc53b8d84b9ad
khamisilawrence4@gmail.com <khamisilawrence@users.noreply.github.com>
2023-10-30 00:42:38 -0500

    added logic

--- gym-typescript-app/src/scenes/contactUs/index.tsx
+++ gym-typescript-app/src/scenes/contactUs/index.tsx
@@ -36,6 +36,25 @@ const ContactUs = ({setSelectedPage}: Props) => {
             Lorem ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris.
           </p>
         </motion.div>
+
+        {/* FORM AND IMAGE */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form action="">
+              .
+            </form>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   )
